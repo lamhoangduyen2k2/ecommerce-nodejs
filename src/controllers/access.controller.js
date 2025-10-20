d54c042c12b1e3a1f9f950795c1543277ad16a50
340056 <duyenit@tythac.com.vn>
2025-10-20 12:30:07 +0700

    [CREATE] Create logout api

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,6 @@
 "use strict";
 
-<<<<<<< HEAD
-import { CREATED } from "../core/success.response.js";
-=======
 import { CREATED, SuccessResponse } from "../core/success.response.js";
->>>>>>> b58800c7ea9332aadacb2127c90db59b1d155d80
 import AccessService from "../services/access.service.js";
 
 class AccessController {
@@ -18,11 +14,7 @@ class AccessController {
       message: "Registered OK!",
       metadata: await AccessService.signUp(req.body),
       options: {
-<<<<<<< HEAD
-        limit: 10
-=======
         limit: 10,
->>>>>>> b58800c7ea9332aadacb2127c90db59b1d155d80
       },
     }).send(res);
   };
